e0e3ef8d9a79d63865557fbeabf1f4df27b8d220
Rami <rami.albadri77@gmail.com>
2019-10-30 12:54:37 +0100

    get players on screen when I start the game and get removed from others screens when I disconnect on server side

--- core/src/frontendServer/server/index.js
+++ core/src/frontendServer/server/index.js
@@ -2,6 +2,8 @@ let app = require('express')();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 
+let players = []
+
 server.listen(8080, () =>
     console.log("Server is running.."));
 
@@ -9,9 +11,26 @@ io.on('connection', (socket) => {
     console.log("Player connected")
 
     socket.emit('socketID', { id: socket.id})
+    socket.emit('getPlayers', players)
     socket.broadcast.emit('newPlayer', { id: socket.id})
-    socket.on('disconnect', () =>
-        console.log("Player disconnected"))
+    socket.on('disconnect', () => {
+        console.log('Player disconnected');
+        socket.broadcast.emit('playerDisconnected', {id: socket.id})
+        players.forEach(s => {
+            if(s.id === socket.id){
+                players.splice(s.index, 1)
+            }
+        })
+    })
+
+    players.push(player(socket.id, (1280 / 2 - 16), (720 / 2 - 32) ))
     });
+
+function player(id, x, y) {
+    this.id = id
+    this.x = x
+    this.y = y
+}
+
 
 
