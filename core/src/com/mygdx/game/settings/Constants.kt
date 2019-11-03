e191be7ed955f704064e7f53101b0379f1661d43
Anders Clark <anders.clark@gmail.com>
2019-11-03 13:06:55 +0100

    Changed player-width

--- core/src/com/mygdx/game/settings/Constants.kt
+++ core/src/com/mygdx/game/settings/Constants.kt
@@ -3,14 +3,14 @@ package com.mygdx.game.settings
 const val WINDOW_WIDTH = 1280f
 const val WINDOW_HEIGHT = 720f
 
-const val MAP_WIDTH = 6400
-const val MAP_HEIGHT = 3600
+const val MAP_WIDTH = 6000
+const val MAP_HEIGHT = 4000
 
 const val PISTOL_RELOAD_TIME = 400L
 const val PISTOL_BULLET_SPEED = 1500f
 const val PLAYER_MOVEMENT_SPEED = 200
 
-const val PLAYER_SPRITE_WIDTH = 32f
+const val PLAYER_SPRITE_WIDTH = 64f
 const val PLAYER_SPRITE_HEIGHT = 64f
 
 const val WALL_SPRITE_WIDTH = 50f
