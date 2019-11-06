5f58713fea74c1a6ec6e4ccc7799368487134d1f
bartoszkruba <bartosz.kruba@gmail.com>
2019-11-06 11:56:28 +0100

    Changed game screen to a little bit bigger

--- core/src/com/mygdx/game/settings/Constants.kt
+++ core/src/com/mygdx/game/settings/Constants.kt
@@ -1,7 +1,7 @@
 package com.mygdx.game.settings
 
-const val WINDOW_WIDTH = 580f
-const val WINDOW_HEIGHT = 420f
+const val WINDOW_WIDTH = 720f
+const val WINDOW_HEIGHT = 720f
 
 const val MAP_WIDTH = 6400
 const val MAP_HEIGHT = 3600
