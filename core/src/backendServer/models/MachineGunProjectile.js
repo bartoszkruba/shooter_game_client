270c039fb5989b8a48a6eda243df063430c530a6
bartoszkruba <bartosz.kruba@gmail.com>
2019-11-18 10:50:14 +0100

    Added damage property to all projectile classes

--- core/src/backendServer/models/MachineGunProjectile.js
+++ core/src/backendServer/models/MachineGunProjectile.js
@@ -5,7 +5,7 @@ const constants = require('../settings/constants');
 class MachineGunProjectile extends Projectile {
     constructor(x = 0, y = 0, xSpeed = 0, ySpeed = 0, id, agentId) {
         super(x, y, xSpeed, ySpeed, constants.STANDARD_PROJECTILE_WIDTH / 2, ProjectileType.MACHINE_GUN,
-            constants.MACHINE_GUN_BULLET_SPEED, id, agentId);
+            constants.MACHINE_GUN_BULLET_SPEED, constants.MACHINE_GUN_PROJECTILE_DAMAGE, id, agentId);
     }
 }
 
